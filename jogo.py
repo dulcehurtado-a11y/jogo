8acdda8c284849f75d463469528f5fa3432d6be7
dudumendez <dulce.hurtado@escola.pr.gov.br>
2026-08-31 08:59:50 -0300

    jogo.py
    Initialize a guessing game with a Tkinter GUI.

--- jogo.py
+++ jogo.py
@@ -1,1 +1,19 @@
+import tkinter as tk
+import random
 
+numero_secreto = 0
+tentativas = 0
+limite = 20
+
+janela = tk.Tk()
+
+janela.title("Jogo de Adivinhação")
+janela.geometry("450x500")
+
+titulo = tk.Label(
+janela,
+text="JOGO DE ADIVINHAÇÃO",
+font=("Arial",20)
+)
+
+titulo.pack(pady=20)
